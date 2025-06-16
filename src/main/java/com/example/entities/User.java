9e6abdbefbbca7dddb9cc81cc73040747cf63100
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 22:47:06 +0200

    Refactor User.java to simplify import statements and enhance code clarity

--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -1,13 +1,15 @@
 package com.example.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany; 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.Set;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity 
+@Table(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
@@ -15,9 +17,17 @@ public class User {
 
   private String name;
 
+  @Column(nullable = false, unique = true)
   private String email;
-
+  @Column(nullable = false)
   private String password;
+
+  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 
   @OneToMany(mappedBy="user")
   private Set<Carte> cartes;
