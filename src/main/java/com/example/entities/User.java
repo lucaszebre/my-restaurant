b64d49515a413f5a5b315e76a275b2854e4fb4a9
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 23:52:29 +0200

    Refactor User.java to organize import statements for clarity

--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -1,12 +1,20 @@
 package com.example.entities;
 
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity 
 @Table(name = "users")
