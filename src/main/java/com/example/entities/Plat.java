7e57c60ba961275e8e8d221810e73f2300fe0267
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 23:52:10 +0200

    Refactor Plat.java to organize import statements for clarity

--- src/main/java/com/example/entities/Plat.java
+++ src/main/java/com/example/entities/Plat.java
@@ -1,8 +1,16 @@
 package com.example.entities;
 
-import jakarta.persistence.*;
-
 import java.util.Set;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 
 @Entity 
