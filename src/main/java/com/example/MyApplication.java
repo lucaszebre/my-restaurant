e82a2f890127ed89992456ce30f083eece529785
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 23:51:08 +0200

    Fix MyApplication.java by reordering import statements for consistency

--- src/main/java/com/example/MyApplication.java
+++ src/main/java/com/example/MyApplication.java
@@ -2,9 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @RestController
 @SpringBootApplication
