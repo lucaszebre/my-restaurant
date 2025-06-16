e9bb675b46698857b86cd21f88078b2af64a0b71
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 22:46:46 +0200

    Refactor Menu.java to simplify import statements and enhance code clarity

--- src/main/java/com/example/entities/Menu.java
+++ src/main/java/com/example/entities/Menu.java
@@ -1,14 +1,10 @@
 package com.example.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany; 
-import jakarta.persistence.ManyToOne; 
-import jakarta.persistence.JoinColumn; 
+import jakarta.persistence.*;
+
 import java.util.Set;
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
+@Table(name = "tables")
 public class Menu {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
