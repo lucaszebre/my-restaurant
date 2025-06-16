7301dfa8f4327d95d79524f2051023d7cd9741d2
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 23:52:17 +0200

    Refactor Review.java to organize import statements for clarity

--- src/main/java/com/example/entities/Review.java
+++ src/main/java/com/example/entities/Review.java
@@ -1,7 +1,14 @@
 package com.example.entities;
 
-import jakarta.persistence.*;
 import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "reviews")
