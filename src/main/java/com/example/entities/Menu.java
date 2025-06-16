6d81d5b4ddd7e4489674c4ebb377361f20a86bad
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 23:52:03 +0200

    Refactor Menu.java to organize import statements for clarity

--- src/main/java/com/example/entities/Menu.java
+++ src/main/java/com/example/entities/Menu.java
@@ -1,8 +1,15 @@
 package com.example.entities;
 
-import jakarta.persistence.*;
-
 import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 @Entity
 @Table(name = "tables")
 public class Menu {
