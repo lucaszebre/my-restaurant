7b4603b967d7866f0f414e0056b21098061396b5
lucaszebre <lucaszebre1@gmail.com>
2025-06-16 22:46:37 +0200

    Refactor Carte.java to simplify import statements and improve readability

--- src/main/java/com/example/entities/Carte.java
+++ src/main/java/com/example/entities/Carte.java
@@ -1,15 +1,11 @@
 package com.example.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany; 
-import jakarta.persistence.ManyToOne; 
-import jakarta.persistence.JoinColumn; 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
+@Table(name = "cartes")
 public class Carte {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
