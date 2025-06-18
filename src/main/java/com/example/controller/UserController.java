fbacae83e1798639b7f0b6302827413af6703bc7
lucaszebre <lucaszebre1@gmail.com>
2025-06-19 00:50:30 +0200

    Add user retrieval methods to UserController for fetching user by ID and listing all users

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,13 +1,34 @@
 package com.example.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
 
 @Controller
 public class UserController {
 
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id ){
+        return userRepository.findById(id).orElseThrow(()->new RuntimeException("no user found"));
+    }
+
+    @GetMapping()
+    public List<User> getUniqUser(){
+        return userRepository.findAll();
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome, this endpoint is not secure";
