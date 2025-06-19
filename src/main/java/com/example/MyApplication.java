8000ad8218f307e80097c7f8bd163eccddc0f969
lucaszebre <lucaszebre1@gmail.com>
2025-06-19 10:04:09 +0200

    Remove unused security configuration and config endpoint from MyApplication

--- src/main/java/com/example/MyApplication.java
+++ src/main/java/com/example/MyApplication.java
@@ -2,13 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-@EnableWebSecurity
 
 public class MyApplication {
 
@@ -17,10 +15,7 @@ public class MyApplication {
 		return "Hello World!";
 	}
 
-	@RequestMapping("/config")
-	public String configPage(){
-		return "config";
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
